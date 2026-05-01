93022f17863942c3c880fbc696ea7d79b7338b63
anaayaar-ops <ana.ayaar@gmail.com>
2026-05-01 03:56:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { WOLF } = wolfjs;
 const settings = {
     identity: process.env.U_MAIL || 'your_email@example.com',
     secret: process.env.U_PASS || 'your_password',
-    taskGroupId: 22271611,
+    taskGroupId: 224,
     depositGroupId: 224,
     minuteInterval: 63 * 1000,
     boxInterval: 3 * 60 * 1000
