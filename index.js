604c7190eec59b15c32793c208859c91150d1358
anaayaar-ops <ana.ayaar@gmail.com>
2026-05-14 00:38:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const { WOLF } = wolfjs;
 const settings = {
     identity: process.env.U_MAIL || 'your_email@example.com',
     secret: process.env.U_PASS || 'your_password',
-    taskGroupId: 22271611,
+    taskGroupId: 224,
     depositGroupId: 224 ,
-    minuteInterval: 63 * 1000,
-    boxInterval: 3 * 60 * 1000
+    minuteInterval: 303 * 1000,
+    boxInterval: 30 * 60 * 1000
 };
  
 const MY_INFO = {
